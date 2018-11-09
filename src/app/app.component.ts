08542e1e69cbe059c9cb144698150a69382411d9
caue <nncl@live.com>
2018-11-09 11:34:25 -0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'testAppZap';
+    @ViewChild('f') form: any;
+
+    // API''
+    data: any = {
+        addresses: [
+            {
+                name: 'Address 1',
+                type: 'commercial',
+                value: 400
+            },
+            {
+                name: 'Address 2',
+                type: 'commercial-2',
+                value: null
+            }
+        ]
+    };
+
+    // APP's
+    addresses: any[] = [
+        {
+            name: 'Address 1',
+            type: 'commercial',
+            value: null
+        },
+        {
+            name: 'Address 2',
+            type: 'commercial-2',
+            value: null
+        }
+    ];
+
+    constructor() {
+        this.data.addresses.map((item) => {
+            if (item.type === 'commercial') {
+                const address = this.addresses.filter((addressItem) => addressItem.type === 'commercial');
+                if (item.value) {
+                    address[0].value = item.value;
+                }
+            }
+
+            if (item.type === 'commercial-2') {
+                const address = this.addresses.filter((addressItem) => addressItem.type === 'commercial-2');
+                if (item.value) {
+                    address[0].value = item.value;
+                }
+            }
+        });
+    }
+
+    request() {
+        console.log(this.addresses);
+    }
 }
